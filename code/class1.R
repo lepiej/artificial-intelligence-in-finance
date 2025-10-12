340ecfe31e99ff5fc05a8602641d6891bddbed9a
Beata Krzemińska <01166048@pw.edu.pl>
2025-10-12 17:19:11 +0200

    Intro to unsupervised learning: k-means clustering on a swiss data matrix

--- code/class1.R
+++ code/class1.R
@@ -5,5 +5,38 @@ hist(Fertility)
 hist(Fertility, col = "red")
 library(Hmisc)
 describe(swiss) 
-hist(Fertility, col = rainbow(20))
-hist(Catholic, col = rainbow(30))
+hist(Fertility, col = rainbow(20), probability = T)
+hist(Catholic, col = rainbow(30), probability = T)
+lines(density(Fertility), lwd=3)
+mean(Fertility)
+sd(Fertility)
+
+hist(Catholic, col = rainbow(20), probability = T)
+lines(density(Catholic), lwd=3)
+
+x <- Agriculture
+hist(x, col = rainbow(20), probability = T)
+lines(density(x), lwd=3)
+sd(x)
+
+install.packages("e1071")
+library(e1071)
+kurtosis(Fertility)
+skewness(Fertility)
+
+summary(swiss)
+
+
+plot(swiss)
+plot(Catholic, Fertility, lwd=2)
+
+results <- kmeans( data.frame(Catholic, Fertility), 2)
+results
+results$cluster
+results$centers
+results$tot.withinss
+
+
+results <- kmeans( data.frame(Catholic, Fertility), 4)
+results$tot.withinss
+plot(Catholic, Fertility, lwd=2, col=results$cluster)
