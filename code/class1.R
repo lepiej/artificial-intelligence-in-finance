83b2e3cb5df4e317cb81216e4225ab446c3d5b03
Beata Krzemińska <01166048@pw.edu.pl>
2025-10-12 15:19:25 +0200

    Into to unsupervised learning: K-means on the Swiss dataset (R).

---
+++ code/class1.R
@@ -0,0 +1,9 @@
+attach(swiss)
+Fertility
+summary(Fertility)
+hist(Fertility)
+hist(Fertility, col = "red")
+library(Hmisc)
+describe(swiss) 
+hist(Fertility, col = rainbow(20))
+hist(Catholic, col = rainbow(30))
