6ea3a509a0d22725488ff5f1e0afb7d7a18c0888
Beata Krzemińska <01166048@pw.edu.pl>
2025-11-23 15:45:15 +0100

    class3

--- code/class1.R
+++ code/class1.R
@@ -1,42 +1,58 @@
-attach(swiss)
-Fertility
-summary(Fertility)
-hist(Fertility)
-hist(Fertility, col = "red")
-library(Hmisc)
-describe(swiss) 
-hist(Fertility, col = rainbow(20), probability = T)
-hist(Catholic, col = rainbow(30), probability = T)
-lines(density(Fertility), lwd=3)
-mean(Fertility)
-sd(Fertility)
+# Load the built-in socio-economic dataset "swiss"
+attach(swiss)                   
 
-hist(Catholic, col = rainbow(20), probability = T)
-lines(density(Catholic), lwd=3)
+#Explore a single variable
+Fertility                        # prints the vector to the console
+summary(Fertility)               # min/1Q/median/mean/3Q/max — quick distribution snapshot
 
+hist(Fertility)                  # basic histogram: shape, spread, outliers
+hist(Fertility, col = "red")     # same histogram with color (purely cosmetic)
+
+# Density overlays help see distribution shape beyond binning
+hist(Fertility, col = rainbow(40), probability = TRUE)  # probability=TRUE → density scale
+hist(Catholic,  col = rainbow(30), probability = TRUE)  # same for "Catholic"
+lines(density(Fertility), lwd = 3)                       # smooth density curve over Fertility histogram
+
+mean(Fertility)                  # central tendency
+sd(Fertility)                    # dispersion (standard deviation)
+
+# Repeat for Catholic with density overlay
+hist(Catholic, col = rainbow(30), probability = TRUE)
+lines(density(Catholic), lwd = 3)
+
+# Another variable: Agriculture
 x <- Agriculture
-hist(x, col = rainbow(20), probability = T)
-lines(density(x), lwd=3)
+hist(x, col = rainbow(20), probability = TRUE)
+lines(density(x), lwd = 3)
 sd(x)
 
-install.packages("e1071")
+# ---- Shape diagnostics: skewness/kurtosis ----
+install.packages("e1071")       # install once; comment out after first run
 library(e1071)
-kurtosis(Fertility)
-skewness(Fertility)
+kurtosis(Fertility)              # tail heaviness vs normal (0 = normal for Fisher definition; here e1071 uses type=3)
+skewness(Fertility)              # asymmetry (>0 right-skewed, <0 left-skewed)
 
-summary(swiss)
+summary(swiss)                   # summary for all variables
 
+# ---- Bivariate relationships ----
+plot(swiss)                      # scatterplot matrix for all pairs — look for linear/nonlinear patterns, clusters
+plot(Catholic, Fertility, lwd=2) # single scatter: how Fertility relates to Catholic
 
-plot(swiss)
-plot(Catholic, Fertility, lwd=2)
+# ---- UNSUPERVISED LEARNING: k-means clustering ----
+# We cluster observations using only features (no labels) → classic unsupervised learning.
+# Here we use 2D space: Catholic (x) and Fertility (y).
 
-results <- kmeans( data.frame(Catholic, Fertility), 2)
-results
-results$cluster
-results$centers
-results$tot.withinss
+results <- kmeans(data.frame(Catholic, Fertility), centers = 2)  # fit 2 clusters
+results                         # prints clustering summary
+results$cluster                 # vector of cluster assignments (1..k) per province
+results$centers                 # centroid coordinates (mean Catholic, mean Fertility for each cluster)
+results$tot.withinss            # total within-cluster sum of squares (compactness measure; lower is better)
 
+# Try more clusters to compare compactness (for elbow heuristic)
+results <- kmeans(data.frame(Catholic, Fertility), centers = 4)
+results$tot.withinss            # compare with k=2; plot tot.withinss for k=1..10 for elbow
 
-results <- kmeans( data.frame(Catholic, Fertility), 4)
-results$tot.withinss
-plot(Catholic, Fertility, lwd=2, col=results$cluster)
+# Visualize clusters in feature space; color by assigned cluster
+plot(Catholic, Fertility, lwd=2, col = results$cluster)
+# Centroids could be added too:
+# points(results$centers, pch = 8, cex = 2, lwd = 2)
--- code/class3.R
+++ code/class3.R
@@ -19,3 +19,127 @@ GENDER <- factor(GENDER)
 
 #dodanie
 DATA <- cbind(DATA, GENDER)
+
+#w ten sposob mamy ustandaryzowane skale w zbiorze 
+#sprawdzamy czy pozostale zmienne w zbiorze moga nam okreslic ryzyko kredytu 'good'/'bad'
+
+#index podzialu 
+library(caret)
+
+index.podz <- createDataPartition (CREDIT.R, times = 1, p=0.8, list=F)
+#zbior treningowy 
+train.data <- DATA[index.podz, ]
+#zbior testowy
+test.data <- DATA[- index.podz, ]
+
+
+#zeby uruchomic model 
+library(e1071)
+
+#model klasyfikacja
+#suprovides vector model gdzie .. oznaczaja wszystko
+#kernel liniowy
+model.klas <- svm(CREDIT.R ~ ., data = train.data,  kernel="linear", cost=1, gamma = 0.1)
+#model przetrenowany 80%
+
+pre.klas <- predict(model.klas, newdata = test.data)
+
+#teraz porownany z danymi rzeczystwistymi na macierzy pomylek albo macierzy rozczarowan 
+cm <- confusionMatrix(pre.klas, test.data$CREDIT.R)
+cm
+#nie nadaje sie do wprowadzenia w zycie bo chce zaproponowac wiecej dobrych kredytow niz zlych, 
+#zupelnie nie rozroznia zlych. kappa to 0 a to rozroznianie nie przez przypadek, 
+#P-Value tez nie powinna byc wysoka (brak informacji)
+#w tym zbiorze brakuje informacji o dochodach 
+#wiec go poprawimy 
+
+
+model.klas <- svm(CREDIT.R ~ ., data = train.data,  kernel="radial", cost=1, gamma = 0.1)
+pre.klas <- predict(model.klas, newdata = test.data)
+cm <- confusionMatrix(pre.klas, test.data$CREDIT.R)
+cm
+#wykrywa juz zle kerdyty
+#kappa wciaz jest male
+
+
+model.klas <- svm(CREDIT.R ~ ., data = train.data,  kernel="polynomial", cost=1, gamma = 0.1)
+pre.klas <- predict(model.klas, newdata = test.data)
+cm <- confusionMatrix(pre.klas, test.data$CREDIT.R)
+cm
+#najwieksze kappa
+
+#jeszcze jeden model do sprawdzenia 
+model.klas <- svm(CREDIT.R ~ ., data = train.data,  kernel="sigmoid", cost=1, gamma = 0.1)
+pre.klas <- predict(model.klas, newdata = test.data)
+cm <- confusionMatrix(pre.klas, test.data$CREDIT.R)
+cm
+#dokladnosc i kappa spadly
+
+#srednio te dane wychodza, zbior pewnie jest przemieszany 
+#to podkrecimy paraemty cost i gamma
+#wprowadzimy nowy obiekt kalibracyjny 
+
+#dodamy cost i gamme jako lista 3x3 kombinacji do przetestowania 
+kalibracja.klas <- tune(svm, train.x = CREDIT.R ~., data = train.data, kernel = "polynomial", ranges = list(cost=c(0.1, 1, 5), gamma=c(0.01, 0.1, 3)) )
+pre.klas <- predict(model.klas, newdata = test.data)
+cm <- confusionMatrix(pre.klas, test.data$CREDIT.R)
+cm
+
+#zmieniamy na kernel = radial
+#dodamy cost i gamme jako lista 3x3 kombinacji do przetestowania 
+kalibracja.klas <- tune(svm, train.x = CREDIT.R ~., data = train.data, kernel = "radial", ranges = list(cost=c(0.1, 1, 5), gamma=c(0.01, 0.1, 3)) )
+
+#mamy rozne wyniki bo przy losowym indeksie mamy inne zmienne w grupie zajeciowej, wiec wyniki sa inne ale zblizone
+
+#zrobmy wykres ktory przedstawi te dane 
+plot(model.klas, data = test.data, Credit_amount ~ Duration)
+
+#mozna uzyc tego samego algorytmu opisujac jedna zmienna 
+#przyklad z modelem zaleznosci 
+#bierzemy prostrzy model klas i przerabiamy go na podobny do regresji dlugosci splaty kredytu 
+model.reg.D <- svm(Duration ~ Credit_amount + Installment_rate + CREDIT.R , data = train.data,  kernel="radial", cost=1, gamma = 0.1)
+
+#przygotowanie predykcji czasu splaty kredytu i klasyfikacji 
+pre.DURATION <- predict (model.reg.D, newdata = test.data )
+
+#korelacja miedzy czasem splaty oczekiwanym a rzeczywistym 
+postResample(pre.DURATION, test.data$Duration)
+#ustawia podstawowe miary ktore sa zroznicowane 
+plot(test.data$Credit_amount, test.data$Duration, col = "blue")
+points(test.data$Credit_amount, pre.DURATION, col = test.data$CREDIT.R, Twd=3)
+#to co czarne to "zly" kredyt -> dluzej splacany 
+
+#wersja z objasnieniem na calym zbiorze 
+model.reg.D <- svm(Duration ~., data = train.data,  kernel="radial", cost=1, gamma = 0.1)
+pre.DURATION <- predict (model.reg.D, newdata = test.data )
+postResample(pre.DURATION, test.data$Duration)
+
+model.reg.D <- svm(Duration ~., data = train.data,  kernel="sigmoid", cost=1, gamma = 0.1)
+pre.DURATION <- predict (model.reg.D, newdata = test.data )
+postResample(pre.DURATION, test.data$Duration)
+#r2 spadl 
+
+plot(test.data$Credit_amount, test.data$Duration, col = "blue")
+points(test.data$Credit_amount, pre.DURATION, col = test.data$CREDIT.R, Twd=3)
+#ten wykres znacznie rozszerzony pionowo, wskazuje bledy 
+
+#czy z korelacji da sie poprawic predykcje? 
+#coreplot 
+library(corrplot)
+install.packages("corrplot") 
+
+View(Credit_DATA_R)
+#przygotowanie macierzy
+M <- cor(data.frame(Credit_DATA_R[ , 1:7]))
+M
+#jedynie kwota kredytu z czasem splaty jest skorelowana znaczaco, cala resta ma korelacje mniejsza niz 0.1 do nawet ujemnych wartosci 
+#test na istotnosc zmiennych
+
+cor.test(Credit_DATA_R$Duration, Credit_DATA_R$Installment_rate)
+corrplot.mixed(M)
+
+#model regresji opisuje zmienne bez klasyfikacji 
+model <- lm(Credit_DATA_R$Duration ~ Credit_DATA_R$Credit_amount + Credit_DATA_R$Installment_rate + as.factor(CREDIT.R) )
+summary(model)
+
+# *** oznaczaja, ze zmienna zawiera istotny parametr 
